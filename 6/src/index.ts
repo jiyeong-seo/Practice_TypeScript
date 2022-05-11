52595d30310fe568f2dee21dc3f38a8e696d88ec
jiyeong-seo <92812508+jiyeong-seo@users.noreply.github.com>
2022-05-12 02:07:36 +0900

    typescript

--- 6/src/index.ts
+++ 6/src/index.ts
@@ -1,15 +1,27 @@
-const filterArrayElements = (array: (number | string)[]): number[] => {
-  const filterdArray: number[] = [];
-  array.forEach((elements) => {
-    if (typeof elements === "string") {
-      filterdArray.push(parseInt(elements, 10));
-    } else {
-      filterdArray.push(elements);
-    }
-  });
-  return filterdArray;
+// const filterArrayElements = (array: (number | string)[]): number[] => {
+//   const filterdArray: number[] = [];
+//   array.forEach((elements) => {
+//     if (typeof elements === "string") {
+//       filterdArray.push(parseInt(elements, 10));
+//     } else {
+//       filterdArray.push(elements);
+//     }
+//   });
+//   return filterdArray;
+// };
+
+// const numberArray = filterArrayElements([1, "2", 3, 4]);
+
+// console.log(numberArray);
+// =======================================================
+const getLastSubject = (subjects: { subject: string | string[] }) => {
+  if (typeof subjects.subject === "object") {
+    const lastItem = subjects.subject.length - 1;
+
+    return subjects.subject[lastItem];
+  } else {
+    return subjects.subject;
+  }
 };
 
-const numberArray = filterArrayElements([1, "2", 3, 4]);
-
-console.log(numberArray);
+console.log(getLastSubject({ subject: ["korean", "art"] }));
