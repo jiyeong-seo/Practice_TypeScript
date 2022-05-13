1aa04a68a807c179b7bb416a1e390f2847b7ba76
jiyeong-seo <92812508+jiyeong-seo@users.noreply.github.com>
2022-05-13 19:02:18 +0900

    TypeScript

--- 7/src/index.ts
+++ 7/src/index.ts
@@ -1,1 +1,15 @@
-type Animal = string | number | number[];
+// type Name = string;
+// type Age = number;
+// // 타입들을 확장
+// // Person 타입에는 string 또는 number 가 할당될 수 있다.
+// type Person = Name | Age;
+//==========================================
+type PositionX = { x: number };
+type PositionY = { y: number };
+
+// { x: number, y: number } 와 같은 타입을 정의하고 싶은  경우
+// object로 선언된 타입들을 하나로 합치고 싶다면
+// & 를 사용하여 하나의 object 타입으로 합칠 수 있다.
+type PositionXY = PositionX & PositionY; // { x: number, y: number }
+
+let position: PositionXY = { x: 10, y: 20 };
--- index.ts
+++
@@ -1,1 +0,0 @@
-type Animal = string | number | number[];
