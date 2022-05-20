e0a76f208f0602c0907b00439d32085194616814
jiyeong-seo <92812508+jiyeong-seo@users.noreply.github.com>
2022-05-20 22:00:54 +0900

    typescript

--- 8/src/index.ts
+++ 8/src/index.ts
@@ -1,17 +1,31 @@
-const rockPaperScissors = (
-  item: "가위" | "바위" | "보"
-): ("가위" | "바위" | "보")[] => {
-  return ["가위"];
+// const rockPaperScissors = (
+//   item: "가위" | "바위" | "보"
+// ): ("가위" | "바위" | "보")[] => {
+//   return ["가위"];
+// };
+
+// const result = rockPaperScissors("가위");
+
+// console.log(result);
+
+// type NumOut = (x: number, y: number) => number;
+
+// let foo: NumOut = (x, y) => {
+//   return x + y;
+// };
+
+// foo(10, 11);
+
+type Member = {
+  plusOne: (x: number) => number;
+  changeName: () => void;
 };
 
-const result = rockPaperScissors("가위");
-
-console.log(result);
-
-type NumOut = (x: number, y: number) => number;
-
-let foo: NumOut = (x, y) => {
-  return x + y;
+let userInfo: Member = {
+  plusOne(x) {
+    return x + 1;
+  },
+  changeName: () => {
+    console.log("안녕");
+  }
 };
-
-foo(10, 11);
